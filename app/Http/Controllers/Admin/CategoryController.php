b0849abad9b5473c37fed0a4ac30c8003c9f6463
Stegura Alex <stegura.alex@gmail.com>
2019-06-12 14:05:36 +0300

    delete category

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -95,6 +95,9 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+
+        return redirect()->route('admin.category.index');
+
     }
 }
--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -12,7 +12,7 @@
 
         <hr>
 
-        <a href="{{route('admin.category.create')}}" class="btn btn-primary pull-right"><i class="fa fa-plus-square-o"></i>Создать категорию</a>
+        <a href="{{route('admin.category.create')}}" class="btn btn-primary pull-right"><i class="fa fa-plus-square-o"></i> Создать категорию</a>
         <table class="table table-striped">
             <thead>
             <th>Наименование</th>
@@ -24,8 +24,16 @@
                     <tr>
                         <td>{{$category->title}}</td>
                         <td>{{$category->published}}</td>
-                        <td>
-                            <a href="{{route('admin.category.edit', $category)}}"><i class="fa fa-edit"></i></a>
+                        <td class="text-right">
+                            <form onsubmit="if (confirm('Удалить?')) { return true}else {return false} " action="{{route('admin.category.destroy', $category)}}" method="post">
+                                <input type="hidden" name="_method" value="DELETE">
+                                {{csrf_field()}}
+
+                                <a class="btn btn-default" href="{{route('admin.category.edit', $category)}}"><i class="fa fa-edit"></i></a>
+
+
+                                <button type="submit" class="btn"><i class="fa fa-trash-o"></i></button>
+                            </form>
                         </td>
                     </tr>
                 @empty
@@ -35,15 +43,15 @@
                 @endforelse
 
             </tbody>
-{{--            <tfoot>--}}
-{{--            <tr>--}}
-{{--                <td colspan="3">--}}
-{{--                    <ul class="pagination pull-right">--}}
-{{--                        {{$categories->links}}--}}
-{{--                    </ul>--}}
-{{--                </td>--}}
-{{--            </tr>--}}
-{{--            </tfoot>--}}
+            {{--            <tfoot>--}}
+            {{--            <tr>--}}
+            {{--                <td colspan="3">--}}
+            {{--                    <ul class="pagination pull-right">--}}
+            {{--                        {{$categories->links}}--}}
+            {{--                    </ul>--}}
+            {{--                </td>--}}
+            {{--            </tr>--}}
+            {{--            </tfoot>--}}
         </table>
     </div>
 
